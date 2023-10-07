9164ee6792a06060543d7c6a90ee0ce14d461070
Joshua Wachana <jjwachana@gmail.com>
2023-10-07 00:17:01 -0700

    Use named function expressions instead of function declarations

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,6 +1,6 @@
 const elem = document.querySelector('input');
 
-function displayText(bool, message) {
+const display = function displayText(bool, message) {
   const outputDiv = document.getElementById('output');
   if (bool) {
     outputDiv.classList.remove('text-danger');
@@ -10,28 +10,28 @@ function displayText(bool, message) {
     outputDiv.classList.add('text-danger');
   }
   outputDiv.innerHTML = message;
-}
+};
 
-function handleInput() {
+const handleInput = function handleInputValues() {
   const enteredValue = document.getElementById('input').value;
 
   if (Number.isNaN(enteredValue)) {
-    displayText(false, 'Error! Non-numeric character(s) entered!');
+    display(false, 'Error! Non-numeric character(s) entered!');
   } else if (enteredValue < 0) {
-    displayText(false, 'Error! Negative number entered!');
+    display(false, 'Error! Negative number entered!');
   } else if (enteredValue === '') {
-    displayText(false, '');
+    display(false, '');
   } else {
     const enteredStringArray = enteredValue.split('');
     const reversedStringArray = enteredStringArray.reverse();
     const reversedString = reversedStringArray.join('');
 
     if (enteredValue === reversedString) {
-      displayText(true, 'Yes! This is a palindrome!');
+      display(true, 'Yes! This is a palindrome!');
     } else {
-      displayText(false, 'No. Try again.');
+      display(false, 'No. Try again.');
     }
   }
-}
+};
 
 elem.addEventListener('input', handleInput);
