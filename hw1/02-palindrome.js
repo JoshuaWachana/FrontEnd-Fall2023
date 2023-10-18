c380493f3877cb0ccfa4ec86460ffa8ccddd830c
Joshua Wachana <jjwachana@gmail.com>
2023-10-18 07:46:05 -0700

    Rework of Palindrome exercise

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,37 +1,47 @@
-const elem = document.querySelector('input');
+const inputElement = document.querySelector('input');
+resultElement = document.querySelector('#result');
 
-const display = function displayText(bool, message) {
-  const outputDiv = document.getElementById('output');
-  if (bool) {
-    outputDiv.classList.remove('text-danger');
-    outputDiv.classList.add('text-success');
-  } else {
-    outputDiv.classList.remove('text-success');
-    outputDiv.classList.add('text-danger');
+const handleInput = () => {
+  resultElement.innerHTML = '';
+  inputNumber = inputElement.value;
+  const isvalidNumber = checkValidityOfNumber(inputNumber);
+  if (!isvalidNumber) {
+    return;
   }
-  outputDiv.innerHTML = message;
+  checkIfPalindrome(inputNumber);
 };
 
-const handleInput = function handleInputValues() {
-  const enteredValue = document.getElementById('input').value;
-
-  if (Number.isNaN(enteredValue)) {
-    display(false, 'Error! Non-numeric character(s) entered!');
-  } else if (enteredValue < 0) {
-    display(false, 'Error! Negative number entered!');
-  } else if (enteredValue === '') {
-    display(false, '');
+const checkValidityOfNumber = (num) => {
+  if (num < 0) {
+    let errMessage = document.createElement('p');
+    errMessage.textContent = 'Invalid Number: It should be positive!';
+    errMessage.classList.add('text-danger', 'bg-white');
+    resultElement.append(errMessage);
+    return false;
+  } else if (num === '') {
+    let errMessage = document.createElement('p');
+    errMessage.textContent = 'Invalid Input!';
+    errMessage.classList.add('text-danger', 'bg-white');
+    resultElement.append(errMessage);
+    return false;
   } else {
-    const enteredStringArray = enteredValue.split('');
-    const reversedStringArray = enteredStringArray.reverse();
-    const reversedString = reversedStringArray.join('');
-
-    if (enteredValue === reversedString) {
-      display(true, 'Yes! This is a palindrome!');
-    } else {
-      display(false, 'No. Try again.');
-    }
+    return true;
   }
 };
 
-elem.addEventListener('input', handleInput);
+const checkIfPalindrome = (num) => {
+  const numString = num.toString();
+  const numStringReversed = num.toString().split('').reverse().join('');
+  let result = document.createElement('p');
+
+  if (numString === numStringReversed) {
+    result.textContent = 'Yes, this is a Palindrome!';
+    result.classList.add('text-success', 'bg-white');
+  } else {
+    result.textContent = 'No, this is not a Palindrome!';
+    result.classList.add('text-danger', 'bg-white');
+  }
+  resultElement.append(result);
+};
+
+inputElement.addEventListener('input', handleInput);
