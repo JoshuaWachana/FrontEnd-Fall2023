bffe49059621efb82cb31e95c3e7d71bd173a138
Joshua Wachana <jjwachana@gmail.com>
2023-11-01 23:47:12 -0700

    ex1 and ex2

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,40 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+const displaySection = document.getElementById('display-section');
 
-fetch(url);
+const addToDOM = (character) => {
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  const figcaption = document.createElement('figcaption');
+  const name = document.createElement('h2');
+  const title = document.createElement('p');
+
+  figure.classList.add('figure');
+  img.classList.add('characterIMG');
+  figcaption.classList.add('figcaption');
+  name.classList.add('name');
+  title.classList.add('title');
+
+  img.setAttribute('src', `${character.imageUrl}`);
+  img.setAttribute('alt', `${character.fullName} from Game of Thrones`);
+  name.textContent = character.fullName;
+  title.textContent = character.title;
+
+  figcaption.append(name, title);
+  figure.append(img, figcaption);
+  displaySection.append(figure);
+};
+
+const getData = async (url) => {
+  try {
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    data.forEach((character) => addToDOM(character));
+  } catch (error) {
+    const errorElement = document.createElement('p');
+    errorElement.textContent = `Sorry an error occurred: ${error}`;
+    displaySection.append(errorElement);
+  }
+};
+
+getData(url);
--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -5,48 +5,128 @@ const backgroundColors = [
   'rgba(75, 192, 192, 0.8)',
   'rgba(153, 102, 255, 0.8)',
   'rgba(255, 159, 64, 0.8)',
-  'rgba(199, 199, 199, 0.8)',
+  'rgba(83, 102, 255, 0.8)',
+  'rgba(78, 52, 199, 0.8)',
+  'rgba(54, 162, 235, 0.8)',
+  'rgba(255, 206, 86, 0.8)',
+  'rgba(255, 99, 132, 0.8)',
+  'rgba(75, 192, 192, 0.8)',
+  'rgba(153, 102, 255, 0.8)',
+  'rgba(255, 159, 64, 0.8)',
   'rgba(83, 102, 255, 0.8)',
   'rgba(40, 159, 64, 0.8)',
   'rgba(210, 199, 199, 0.8)',
   'rgba(78, 52, 199, 0.8)',
+  'rgba(54, 162, 235, 0.8)',
+  'rgba(255, 206, 86, 0.8)',
+  'rgba(255, 99, 132, 0.8)',
+  'rgba(75, 192, 192, 0.8)',
+  'rgba(153, 102, 255, 0.8)',
+  'rgba(255, 159, 64, 0.8)',
 ];
 
-const borderColors = [
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(255, 99, 132, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
-  'rgba(159, 159, 159, 1)',
-  'rgba(83, 102, 255, 1)',
-  'rgba(40, 159, 64, 1)',
-  'rgba(210, 199, 199, 1)',
-  'rgba(78, 52, 199, 1)',
-];
+const borderColors = [...backgroundColors];
 
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-const renderChart = () => {
+const renderChart = (houseData) => {
   const donutChart = document.querySelector('.donut-chart');
+  const chartLegend = document.querySelector('.chart-legend');
+
+  // const donutChart = document.getElementById('chart-1').getContext('2d');
+
+  console.log(houseData);
+
+  let listOfHouses = [];
+  let numberOfMembersList = [];
+
+  let indexColor = 0;
+
+  for (house in houseData) {
+    listOfHouses.push(house);
+    const key = document.createElement('div');
+    key.innerHTML = `<div class="key-container"><div class="key-color-box" style="background-color: ${backgroundColors[indexColor]}"></div><div class="key">${house}</div></div>`;
+    chartLegend.append(key);
+    numberOfMembersList.push(houseData[house]);
+    indexColor += 1;
+  }
 
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: [...listOfHouses],
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          label: 'Members per House',
+          data: [...numberOfMembersList],
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
         },
       ],
     },
+    options: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: 'Members Per House',
+        fontSize: 20,
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+    },
   });
 };
 
-renderChart();
+const calculateHouseMembers = (characters) => {
+  const namesAndHousesCleaned = characters.map((character) => {
+    const originalHouseName = character.family;
+    let houseNameEdited = originalHouseName.replaceAll('House ', '');
+    if (houseNameEdited.includes('Targa')) {
+      houseNameEdited = 'Targaryen';
+    }
+    if (houseNameEdited.includes('Lan')) {
+      houseNameEdited = 'Lannister';
+    }
+    if (houseNameEdited.includes('Lora')) {
+      houseNameEdited = 'Lorathi';
+    }
+
+    if (
+      houseNameEdited === 'Unknown' ||
+      houseNameEdited === 'Unkown' ||
+      houseNameEdited === '' ||
+      houseNameEdited === 'None'
+    ) {
+      houseNameEdited = 'Other';
+    }
+    return { name: `${character.fullName}`, house: `${houseNameEdited}` };
+  });
+
+  const membersPerHouseObj = namesAndHousesCleaned.reduce(
+    (total, character) => {
+      if (total.hasOwnProperty(character.house)) {
+        total[character.house] += 1;
+      } else {
+        total[character.house] = 1;
+      }
+      return total;
+    },
+    {}
+  );
+
+  renderChart(membersPerHouseObj);
+};
+
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    calculateHouseMembers(data);
+  } catch (error) {}
+};
+
+getData(url);
